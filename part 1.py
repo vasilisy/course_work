cda38252d0ceb7291482adb96428d455148f5476
vasilisy <m2009903@edu.misis.ru>
2023-05-03 15:40:22 +0300

    update in request from google street view

--- part 1.py
+++ part 1.py
@@ -5,7 +5,7 @@ import shutil
 
 
 def save_image(coords, name):
-    res_image = requests.get('https://maps.googleapis.com/maps/api/streetview?size=2400x2400&location='+coords+'&radius=1234&key=AIzaSyAjSPCl4G3JOz8cXWL5GGiczLQHeVV37D8', stream=True)
+    res_image = requests.get('https://maps.googleapis.com/maps/api/streetview?size=600x300&location='+coords+'&pitch=0&fov=120&radius=10&key=AIzaSyAjSPCl4G3JOz8cXWL5GGiczLQHeVV37D8', stream=True)
     with open('results/'+name, 'wb') as f:
         shutil.copyfileobj(res_image.raw, f)
 
@@ -38,9 +38,10 @@ for i in output:
 # проверка, есть ли панорама/изображение в google steert view и если есть, то сохранить изображение
 count = 0
 for coords in output:
-    res = requests.get('https://maps.googleapis.com/maps/api/streetview/metadata?size=600x300&location='+coords+'&heading=-45&pitch=42&fov=110&key=AIzaSyAjSPCl4G3JOz8cXWL5GGiczLQHeVV37D8')
+    res = requests.get('https://maps.googleapis.com/maps/api/streetview/metadata?size=600x300&location='+coords+'&radius=10&key=AIzaSyAjSPCl4G3JOz8cXWL5GGiczLQHeVV37D8')
     res_json = json.loads(res.text)
     if res_json.get('status') == 'OK':
         count += 1
         name = 'image_'+str(count)+'.jpg'
         save_image(coords, name)
+print(count)
